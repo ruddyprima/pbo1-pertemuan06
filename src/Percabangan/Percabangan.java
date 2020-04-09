b57c3be890bc527d5241e37d413ab6813c5c10b0
ruddyprima <62361121+ruddyprima@users.noreply.github.com>
2020-04-09 20:19:24 +0700

    Percabangan if dan else

--- src/Percabangan/Percabangan.java
+++ src/Percabangan/Percabangan.java
@@ -5,7 +5,7 @@ public class Percabangan {
         double totalBelanja = 53000;
         double uangDidompet = 25000;
 
-        if (uangDidompet < totalBelanja) {
+        if(uangDidompet<totalBelanja) {
             System.out.println("Uang anda kurang, kurangi jajan anda");
         } else {
             System.out.println("Uang cukup, selamat menikmati");
