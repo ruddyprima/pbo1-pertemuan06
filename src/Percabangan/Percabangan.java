349fecc9668e58cd5763a7980506bdb753f4fad6
ruddyprima <62361121+ruddyprima@users.noreply.github.com>
2020-04-09 20:17:30 +0700

    Percabangan if dan else

---
+++ src/Percabangan/Percabangan.java
@@ -0,0 +1,15 @@
+package Percabangan;
+
+public class Percabangan {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
+
+        if (uangDidompet < totalBelanja) {
+            System.out.println("Uang anda kurang, kurangi jajan anda");
+        } else {
+            System.out.println("Uang cukup, selamat menikmati");
+        }
+    }
+    }
+}
