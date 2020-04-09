2c7a93ff43a1ecdb55320034ef6bdcfc43c4e8a3
ruddyprima <62361121+ruddyprima@users.noreply.github.com>
2020-04-09 20:49:43 +0700

    Percabangan Switch 2

---
+++ src/Percabangan/Switch2.java
@@ -0,0 +1,30 @@
+package Percabangan;
+
+public class Switch2 {
+    public static void main(String[] args) {
+        char nilai='D';
+        switch(nilai){
+            case 'A':
+                System.out.println("Mahasiswa : \"terima kasih pak\"");
+                System.out.println("Dosen : \"Selamat ya\"");
+                break;
+            case 'B':
+                System.out.println("Mahasiswa : \"Kenapa ulun kada dapat A, Pak?\"");
+                System.out.println("Dosen : \"!@#$\"");
+                break;
+            case 'C':
+                System.out.println("Mahasiswa : \"Ulun Turun Pul Pak ae, Tugas pul jua\"");
+                System.out.println("Dosen : \"Tapi bisa lah menjawab Ujian?\"");
+                        System.out.println("Mahasiswa : \"Hehehe\"");
+                   break;
+            case 'C':
+                System.out.println("Mahasiswa : \"Ulun Turun Pul Pak ae, Tugas pul jua\"");
+                System.out.println("Dosen : \"Bujur jua kh?\"");
+                System.out.println("Dosen : \"Memeriksa Berkas\"");
+                System.out.println("Dosen : \"Menceleng\"");
+                System.out.println("Mahasiswa : \"Kabur\"");
+                break;
+        }
+    }
+
+}
